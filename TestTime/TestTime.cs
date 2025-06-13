5c91cb207b936c8a8910f5342804cba3fad674cc
Takeduh <tanjr.buenrico07@gmail.com>
2025-06-13 10:04:41 +0800

    Final Update: Updated to Activity 6

--- TestTime/TestTime.cs
+++ TestTime/TestTime.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TestTime;
 
 namespace TestTime
 {
@@ -6,92 +7,97 @@ namespace TestTime
     {
         private readonly int minutes;
 
-        // Constructor with hours and minutes
         public Time(int hh, int mm)
         {
             this.minutes = 60 * hh + mm;
         }
-
-        // Additional constructor that takes total minutes
+        
         public Time(int totalMinutes)
         {
-            // Ensure we wrap around for times exceeding 24 hours
             this.minutes = totalMinutes % (24 * 60);
 
-            // Handle negative times by adding 24 hours until positive
             while (this.minutes < 0)
             {
                 this.minutes += 24 * 60;
             }
         }
 
-        // Read-only property Hour returning the number of hours
         public int Hour
         {
             get { return minutes / 60; }
         }
 
-        // Read-only property Minute returning the number of minutes
         public int Minute
         {
             get { return minutes % 60; }
         }
 
-        // Overload + operator to add two Time values
         public static Time operator +(Time t1, Time t2)
         {
             return new Time(t1.minutes + t2.minutes);
         }
 
-        // Overload - operator to subtract two Time values
         public static Time operator -(Time t1, Time t2)
         {
             return new Time(t1.minutes - t2.minutes);
         }
 
+        // Implicit conversion from int (minutes since midnight) to Time
+        public static implicit operator Time(int minutes)
+        {
+            return new Time(minutes);
+        }
+
+        // Explicit conversion from Time to int (minutes since midnight)
+        public static explicit operator int(Time time)
+        {
+            return time.minutes;
+        }
+
         public override String ToString()
         {
-            // Format as hh:mm with leading zeros using String.Format
             return String.Format("{0:D2}:{1:D2}", Hour, Minute);
         }
     }
+}
+
 
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // Declare variables of type Time
-            Time morning = new Time(10, 5);    // 10:05
-            Time midday = new Time(12, 0);     // 12:00
-            Time evening = new Time(23, 45);   // 23:45
+        Time morning = new Time(10, 5);
+        Time midday = new Time(12, 0);
+        Time evening = new Time(23, 45);
 
-            // Print the Time values
-            Console.WriteLine("Morning time: " + morning);
-            Console.WriteLine("Midday time: " + midday);
-            Console.WriteLine("Evening time: " + evening);
+        Console.WriteLine("Morning time: " + morning);
+        Console.WriteLine("Midday time: " + midday);
+        Console.WriteLine("Evening time: " + evening);
 
-            // Use the overloaded + operator
-            Time sum = morning + midday;
-            Console.WriteLine($"{morning} + {midday} = {sum}");  // Should be 22:05
+        Time sum = morning + midday;
+        Console.WriteLine($"{morning} + {midday} = {sum}");
 
-            // Use the overloaded - operator
-            Time difference = evening - morning;
-            Console.WriteLine($"{evening} - {morning} = {difference}");  // Should be 13:40
+        Time difference = evening - morning;
+        Console.WriteLine($"{evening} - {morning} = {difference}");
 
-            // Demonstrate time wrapping around
-            Time lateNight = new Time(23, 30);
-            Time hourAndHalf = new Time(1, 30);
-            Time wrappedTime = lateNight + hourAndHalf;
-            Console.WriteLine($"{lateNight} + {hourAndHalf} = {wrappedTime}");  // Should be 01:00
+        // Using the implicit conversion from int to Time
+        int lunchTimeMinutes = 720;
+        Time lunch = lunchTimeMinutes;
+        Console.WriteLine($"Implicit conversion from {lunchTimeMinutes} minutes to Time: {lunch}");
 
-            // Use the Time(int) constructor
-            Time fromMinutes = new Time(505);  // 8:25
-            Console.WriteLine("Time from 505 minutes: " + fromMinutes);
+        // Using the explicit conversion from Time to int
+        int morningMinutes = (int)morning;
+        Console.WriteLine($"Explicit conversion from Time {morning} to minutes: {morningMinutes}");
 
-            // Demonstrate negative time handling
-            Time negativeResult = morning - evening;
-            Console.WriteLine($"{morning} - {evening} = {negativeResult}");  // Should wrap around
-        }
+        // Using conversions in calculations
+        Time breakTime = 570;
+        int minutesWorked = (int)(morning - breakTime);
+        Console.WriteLine($"Minutes worked from {breakTime} to {morning}: {minutesWorked}");
+
+        Time t1 = 505;
+        Console.WriteLine($"Time t1 from 505 minutes: {t1}");
+
+        int t2Minutes = (int)t1;
+        Console.WriteLine($"Minutes in t1: {t2Minutes}");
     }
-
 }
